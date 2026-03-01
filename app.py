7b72be182d3cd7b25363b19e1fd39ddf6183b0a6
Anasteziya0 <nastya_danilova2006@mail.ru>
2026-03-01 11:20:24 +0300

    1.5

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI
-from models import User
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from models import User, UserAge
 
 app = FastAPI()
 
-user = User(name="Анастасия Данилова", id=1)
+user = User(name="Данилова Анастасия", id=1)
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+@app.get("/")
+async def root():
+    return FileResponse("index.html")
+
+@app.post("/calculate")
+async def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {"result": result}
 
 @app.get("/users")
-def get_user():
+async def get_user():
     return user
+
+@app.post("/user")
+async def check_user(user: UserAge):
+    is_adult = user.age >= 18
+    return {
+        "name": user.name,
+        "age": user.age,
+        "is_adult": is_adult
+    }
--- models.py
+++ models.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class User(BaseModel):
     name: str
     id: int
+
+class UserAge(BaseModel):
+    name: str
+    age: int
